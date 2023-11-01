78311a6947dfa61e21147969bf0c8ec9aefb8998
Christian Ani <iamchristianani@gmail.com>
2023-11-01 20:01:01 +0100

    Create Home page to hold all components

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+import { Header, Companies, Service } from './';
+
+const Home = () => (
+    <Box>
+        <Header />
+        <Companies />
+        <Service />
+    </Box>
+)
+
+export default Home
