8870beb9b0479cf83158a6b6004bb30cb39eeec2
Christian Ani <iamchristianani@gmail.com>
2023-11-03 10:14:11 +0100

    Create and style pricing section

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Home } from './Home';
 export { default as Header } from './Header';
 export { default as Companies } from './Companies';
 export { default as Service } from './Service';
+export { default as Portfolio } from './Portfolio';
+export { default as Pricing } from './Pricing';
